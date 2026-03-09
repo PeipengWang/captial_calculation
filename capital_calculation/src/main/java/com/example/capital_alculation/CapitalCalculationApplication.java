4d1918097ecdf9f7f3ea29ca660233691a402b1d
PeipengWang <wangpeipeng123@163.com>
2026-03-09 20:52:38 +0800

    first commit

---
+++ capital_calculation/src/main/java/com/example/capital_alculation/CapitalCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.example.capital_alculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalCalculationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapitalCalculationApplication.class, args);
+    }
+
+}
